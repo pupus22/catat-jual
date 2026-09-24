81a44525047e4c7e44cf8e6b007db17143e6d1ee
soerjo.91 <soerjo.91@gmail.com>
2026-09-24 11:04:59 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
-// Isi dengan konfigurasi Web App dari Firebase Console > Project settings > Your apps.
-// apiKey Firebase Web BUKAN password rahasia. Amankan data melalui Authentication + Firestore Rules.
-export const firebaseConfig = {
-  apiKey: "ISI_API_KEY",
-  authDomain: "ISI_PROJECT_ID.firebaseapp.com",
-  projectId: "ISI_PROJECT_ID",
-  storageBucket: "ISI_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "ISI_MESSAGING_SENDER_ID",
-  appId: "ISI_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyB4eUuMFP6FESjTySUegaax9YPWdrcjj-o",
+  authDomain: "cangkang-mas.firebaseapp.com",
+  projectId: "cangkang-mas",
+  storageBucket: "cangkang-mas.firebasestorage.app",
+  messagingSenderId: "710784251234",
+  appId: "1:710784251234:web:5878ece0eb49844116283b"
 };
